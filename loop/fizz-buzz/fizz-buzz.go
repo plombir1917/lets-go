5ac81dfb65a3841e098145e7c472549208a89819
KopichZS <KopichZS@admlr.lipetsk.ru>
2025-07-28 13:52:24 +0300

    fix(fizz-buzz): add FizzBuzz condition

--- loop/fizz-buzz/fizz-buzz.go
+++ loop/fizz-buzz/fizz-buzz.go
@@ -4,13 +4,19 @@ import "fmt"
 
 func main() {
 	for i := 1; i <101; i ++ {
-		switch {
-		case (i % 3) == 0:
-			fmt.Println("Fizz")
-		case i %5 == 0:
-			fmt.Println("Buzz")
-		default: 
-			fmt.Println(i)
+		found := false
+		if (i % 3) == 0 {
+			fmt.Printf("Fizz")
+			found = true
 		}
+		if (i % 5) == 0 { 
+			fmt.Printf("Buzz")
+			found = true
+		}
+		if !found { 
+			fmt.Println(i)
+			continue
+		}
+			fmt.Println()
 	}
 }
