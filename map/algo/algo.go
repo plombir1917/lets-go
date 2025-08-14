e82e152d6967fea483447af7d42231ec38993784
KopichZS <KopichZS@admlr.lipetsk.ru>
2025-08-14 17:26:51 +0300

    feat: algo examples

---
+++ map/algo/algo.go
@@ -0,0 +1,48 @@
+package main
+
+import "fmt"
+
+func find(arr []int, k int) []int {
+	// Создадим пустую map
+	m := make(map[int]int)
+	// будем складывать в неё индексы массива, а в качестве ключей использовать само значение
+	for i, a := range arr {
+		if j, ok := m[k-a]; ok { // если значение k-a уже есть в массиве, значит, arr[j] + arr[i] = k и мы нашли, то что нужно
+			return []int{i, j}
+		}
+		// если искомого значения нет, то добавляем текущий индекс и значение в map
+		m[a] = i
+	}
+	// не нашли пары подходящих чисел
+	return nil
+}
+
+// как можно заметить, алгоритм пройдётся по массиву всего один раз
+// если бы мы искали подходящее значение каждый раз через перебор массива, то пришлось бы сделать гораздо больше вычислений
+
+func removeDuplicates(input []string) []string {
+    output := make([]string,0)
+    inputSet := make(map[string]struct{}, len(input))
+    for _, v := range input {
+        if _, ok := inputSet[v]; !ok {
+            output = append(output, v)
+
+        }
+        inputSet[v] = struct{}{}
+    }
+
+    return output
+}
+
+func main() {
+	input := []string{
+    "cat",
+    "dog",
+    "bird",
+    "dog",
+    "parrot",
+    "cat",
+} 
+	fmt.Println(find([]int{1: 1, 2: 2, 3: 3, 4: 4, 5: 5}, 5))
+	fmt.Println(removeDuplicates(input))
+}
