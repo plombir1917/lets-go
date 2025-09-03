aae534a8d38ec3ffa518ff5b725c0658159a42fb
KopichZS <KopichZS@admlr.lipetsk.ru>
2025-09-03 16:54:55 +0300

    feat: struct

---
+++ struct/struct.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Person struct {
+	// теги
+	Name  string `json:"Имя" example:"Alex"`
+	Email string `json:"Почта" example:"мыло"`
+	Age   int    `json:"-"`
+}
+
+func main() {
+	p := Person{
+		Name:  "Лёха",
+		Email: "мыло", // ЗАПЯТАЯ СТАВИТСЯ НА ПОСЛЕДНЕМ СВОЙСТВЕ ВСЕГДА!!!
+	}
+
+	secondPerson := NewPerson("Имя", "мыло", 12)
+
+	fmt.Println(p, secondPerson, p.Name)
+}
+
+// Аналог конструктора
+func NewPerson(name, email string, Age int) Person {
+	return Person{name, email, Age}
+}
