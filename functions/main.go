9a4b386c806785febeed83f24749c7a14dcef122
KopichZS <KopichZS@admlr.lipetsk.ru>
2025-11-21 16:30:04 +0300

    feat(functions): example

---
+++ functions/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func main() {
+	PrintAllFiles(".", "main")
+}
+
+func PrintAllFiles(path string, filter string) {
+	files, err := os.ReadDir(path)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for _, f := range files {
+		filename := filepath.Join(path, f.Name())
+		
+		if strings.Contains(filename, filter) {
+			fmt.Println(filename)
+		}
+
+		if f.IsDir() {
+			PrintAllFiles(filename, filter)
+		}
+	}
+}
